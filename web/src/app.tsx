a94e5992d08e5b44d56a311b2651be07a59273c5
RichardPMachado <richardmachado2211@gmail.com>
2023-09-20 23:44:08 -0300

    fefactor: Renamed App' to 'app'

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,10 +1,11 @@
 import { Button } from "./components/ui/button";
-import { Github, FileVideo, Upload, Wand2 } from "lucide-react"
+import { Github, Wand2 } from "lucide-react"
 import { Separator } from "./components/ui/separator";
 import { Textarea } from "./components/ui/textarea";
 import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@radix-ui/react-select";
 import { Slider } from "./components/ui/slider";
+import { VideoInputForm } from "./components/video-input-form";
 
 export function App() {
 
@@ -42,28 +43,7 @@ export function App() {
           </p>
         </div>
         <aside className="w-80 space-y-6">
-          <form className="space-y-6">
-            <label 
-              htmlFor="video"
-              className="border flex flex-col items-center justify-center text-sm rounded-md aspect-video gap-2 cursor-pointer border-dashed text-muted-foreground hover:bg-primary/5">
-                <FileVideo className="w-4 h-4" />
-                Selecione um video
-            </label>
-            <input type="file" id="video" accept="video/mp4" className="sr-only" />
-            <Separator />
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
-              <Textarea
-                id="transcription_prompt"
-                className="h-20 leading-relaxed resize-none"
-                placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)"  
-              />
-            </div>
-            <Button type="submit" className="w-full">
-              Carregar vídeo
-              <Upload className="h-4 w-4 ml-2" />
-            </Button>
-          </form>
+          <VideoInputForm />
 
           <Separator />
 
